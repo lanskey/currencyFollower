adb44216f0978ec40bb1a9df2105790acd367a36
Emanuel Slotwinski <ask@eslotwinski.com>
2017-02-14 22:15:34 +0100

    Update tests

--- app/containers/SelectCurrency/tests/index.test.js
+++ app/containers/SelectCurrency/tests/index.test.js
@@ -1,5 +1,6 @@
 import { SelectCurrency } from '../index';
 import { shallow } from 'enzyme'
+import sinon from 'sinon'
 
 describe('<SelectCurrency />', () => {
   let props
@@ -20,9 +21,17 @@ describe('<SelectCurrency />', () => {
     renderComponent.instance().onClickHandler(1)()
     expect(renderComponent.state().followedCurrencies).to.eql([])
   })
-  
+
   it('should render CurrencyList component', () => {
     const expected = renderComponent.find('CurrencyList')
     expect(expected).to.have.length.of(1)
+  })
+
+  it('should mark selected currency component', () => {
+    const mockMarkActive = sinon.mock()
+    renderComponent.setProps({ markActive: mockMarkActive })
+
+    renderComponent.instance().markAsSelected()()
+    expect(mockMarkActive.calledOnce).to.eql(true)
   })
 })
