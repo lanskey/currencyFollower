b68e05aac169144b397a98acb1a0069207ba8798
Emanuel Slotwinski <ask@eslotwinski.com>
2017-02-14 18:47:17 +0100

    Add SelectCurrency container

---
+++ app/containers/SelectCurrency/index.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { addCurrency } from 'actions/addCurrency'
+import CurrencyList from 'components/CurrencyList'
+
+
+export class SelectCurrency extends React.Component {
+  state = {
+    activeCurrencies: []
+  }
+
+  onClickHandler = (index) => {
+    return () => {
+      const { activeCurrencies } = this.state
+
+      // If element is already in array remove it (deactivate)
+      // else mark it as active
+      if (activeCurrencies.indexOf(index) >= 0) {
+        const stateIndex = activeCurrencies.indexOf(index)
+        const newSelectedCurrencies = activeCurrencies.filter((item, index) => index !== stateIndex)
+
+        this.setState({ activeCurrencies: newSelectedCurrencies })
+      }
+      else {
+        this.setState({ activeCurrencies: [...activeCurrencies, index] })
+      }
+    }
+  }
+
+  render () {
+    console.log(this.state.activeCurrencies)
+    return (
+      <div className="container-fluid">
+        <CurrencyList items={this.props.currencies} activeElements={this.state.activeCurrencies} onClick={this.onClickHandler} />
+      </div>
+    );
+  }
+}
+
+export default SelectCurrency
---
+++ app/containers/SelectCurrency/tests/index.test.js
@@ -0,0 +1,19 @@
+import { SelectCurrency } from '../index';
+import { Provider } from 'react-redux'
+import store from '../../../store'
+import { mount } from 'enzyme'
+
+describe('addCurrency', () => {
+  let props
+  let renderComponent;
+  beforeEach(() => {
+    props = {
+      currency: ['1', '2']
+    }
+    renderComponent = mount(
+      <Provider store={store}>
+        <SelectCurrency {...props} />
+      </Provider>
+    );
+  });
+})
